5a36e784b3bcf62d953a51ca9e3a52bd5b799767
DiosAkhilleus <abertin1809@gmail.com>
2021-07-23 17:22:52 -0500

    added id submission to request timeslot info from db

--- client/src/Components/Timeslot/ViewTimeslot.jsx
+++ client/src/Components/Timeslot/ViewTimeslot.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { getTimeslotByUUID } from '../../javascript/timeslotLogic';
 import '../../App.css';
 
@@ -28,6 +29,7 @@ const ViewTimeslot = () => {
   return (
     <div className="timeslot-search-container">
       <h1>Enter Timeslot ID below to access inspection signup</h1>
+      <Link to='/'>Back to home</Link>
       <input
         className="timeslot-search-bar"
         type="text"
