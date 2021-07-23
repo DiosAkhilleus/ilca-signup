01bd918360e313efaa376819fb8030b56a1d020a
DiosAkhilleus <abertin1809@gmail.com>
2021-07-23 14:04:19 -0500

    added new Link

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import About from './Components/About.jsx';
 import NotFound from './Components/NotFound.jsx';
 import Timeslot from './Components/Timeslot/Timeslot.jsx';
 import TimeslotPost from './Components/Timeslot/TimeslotPost.jsx';
+import ViewTimeslot from './Components/Timeslot/ViewTimeslot.jsx';
 import PostSailor from './Components/Sailor/PostSailor.jsx';
 import DisplaySailors from './Components/Sailor/DisplaySailors.jsx';
 
@@ -18,6 +19,7 @@ function App() {
         <Route exact path='/about' component={About} />
         <Route exact path='/timeslots' component={Timeslot} />
         <Route exact path='/posttimeslot' component={TimeslotPost} />
+        <Route exact path='/viewtimeslot' component={ViewTimeslot}/>
         <Route exact path='/postsailor' component={PostSailor} />
         <Route exact path='/displaysailors' component={DisplaySailors} />
         <Route path='/' component={NotFound}/>
