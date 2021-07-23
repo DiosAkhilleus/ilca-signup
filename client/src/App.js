1e4f36f9be44decaaac866c694564aac4eee92a1
DiosAkhilleus <abertin1809@gmail.com>
2021-07-22 22:19:54 -0500

    added new route

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home.jsx'
 import About from './Components/About.jsx';
 import NotFound from './Components/NotFound.jsx';
-import Timeslot from './Components/Timeslot.jsx';
+import Timeslot from './Components/Timeslot/Timeslot.jsx';
+import TimeslotPost from './Components/Timeslot/TimeslotPost.jsx';
 import PostSailor from './Components/Sailor/PostSailor.jsx';
 import DisplaySailors from './Components/Sailor/DisplaySailors.jsx';
 
@@ -15,7 +16,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
-        <Route exact path='/timeslot' component={Timeslot}/>
+        <Route exact path='/timeslots' component={Timeslot} />
+        <Route exact path='/posttimeslot' component={TimeslotPost} />
         <Route exact path='/postsailor' component={PostSailor} />
         <Route exact path='/displaysailors' component={DisplaySailors} />
         <Route path='/' component={NotFound}/>
