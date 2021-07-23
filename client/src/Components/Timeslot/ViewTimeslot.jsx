e6bb5795c9d24ea8e47a32d465c78746f4660bec
DiosAkhilleus <abertin1809@gmail.com>
2021-07-23 14:05:49 -0500

    added new timeslot view component

---
+++ client/src/Components/Timeslot/ViewTimeslot.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { getTimeslotByUUID } from '../../javascript/timeslotLogic';
+import '../../App.css';
+
+const ViewTimeslot = () => {
+  const [UUID, setUUID] = useState('');
+  const [currentTimeslots, setCurrentTimeslots] = useState([]);
+  const [selectedTimeslot, setSelectedTimeslot] = useState({});
+
+  useEffect(() => {
+    getTimeslotByUUID().then((results) => setCurrentTimeslots(results));
+  }, []);
+
+  useEffect(() => {
+    console.log(selectedTimeslot);
+  }, [selectedTimeslot])
+
+  const handleIDSubmission = () => {
+    const filteredTimeslots = currentTimeslots.filter(timeslot => timeslot.uuid === UUID);
+    if (! filteredTimeslots.length > 0) {
+      alert(`This id doesn't match a registered Inspection Signup`);
+    } else {
+      setSelectedTimeslot(filteredTimeslots[0]);      
+    }
+  };
+
+  return (
+    <div className="timeslot-search-container">
+      <h1>Enter Timeslot ID below to access inspection signup</h1>
+      <input
+        className="timeslot-search-bar"
+        type="text"
+        onChange={(e) => setUUID(e.target.value)}
+      />
+      <button onClick={() => handleIDSubmission()}>Submit ID</button>
+    </div>
+  );
+};
+
+export default ViewTimeslot;
