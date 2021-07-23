33e4c50e2b3577bbe4f0ad7cc32d8e7f297c61d1
DiosAkhilleus <abertin1809@gmail.com>
2021-07-22 22:22:40 -0500

    added new routes

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -8,9 +8,10 @@ const Home = () => {
   return (
     <div className="home-page">
       <h1>Home Page</h1>
-      <Link to='/timeslot'>Time Slot Selector</Link>
+      <Link to='/timeslots'>Time Slot Selector</Link>
+      <Link to='/posttimeslot'>Post Time Slot</Link>
       <Link to='/postsailor'>New Sailor Form</Link>
-      <Link to='/displaysailors'>Show all entered sailors</Link>
+      <Link to='/displaysailors'>Show all entered sailors</Link> 
     </div>
   );
 };
