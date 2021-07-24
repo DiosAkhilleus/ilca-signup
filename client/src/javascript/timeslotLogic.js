dde9a3e362603e3f4bc00a994c3f1efcf7eb6424
DiosAkhilleus <abertin1809@gmail.com>
2021-07-24 16:18:45 -0500

    added POST function to update timeslots

--- client/src/javascript/timeslotLogic.js
+++ client/src/javascript/timeslotLogic.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
-export const postTimeslotReqToDB = (sailorID, firstName, familyName, time, day) => {
+export const postTimeslotReqToDB = (eventTitle, sailorID, firstName, familyName, time, day) => {
   Axios.post('http://localhost:3001/reqtimeslot', {
+    eventTitle: eventTitle,
     sailorID: sailorID, 
     name: {
       firstName: firstName,
@@ -13,25 +14,18 @@ export const postTimeslotReqToDB = (sailorID, firstName, familyName, time, day) 
 }
 
 export const getCurrentlyScheduledInspections = () => {
-  const retrieve = Axios.get('http://localhost:3001/timeslots/filled').then((response) => {
+  const res = Axios.get('http://localhost:3001/timeslots/filled').then((response) => {
     return response.data;
   });
-  return retrieve;
+  return res;
 }
 
-export const postCreatedTimeslotToDB = (unavailableSlots, interval, selectedDates, eventTitle, timeFrom, timeTo, uuid) => {
-  console.log({
-    unavailableSlots: unavailableSlots, 
-    interval: interval,
-    selectedDates: selectedDates,
-    eventTitle: eventTitle,
-    timeFrom: timeFrom,
-    timeTo: timeTo ,
-    uuid: uuid
-  })
+export const postCreatedTimeslotToDB = (slotsAvailableByDay, unavailableSlots, interval, entryLimit, selectedDates, eventTitle, timeFrom, timeTo, uuid) => {
   Axios.post('http://localhost:3001/timeslots/created/', {
+    slotsAvailableByDay: slotsAvailableByDay,
     unavailableSlots: unavailableSlots, 
     interval: interval,
+    entryLimit: entryLimit,
     selectedDates: selectedDates,
     eventTitle: eventTitle,
     timeFrom: timeFrom,
@@ -41,8 +35,27 @@ export const postCreatedTimeslotToDB = (unavailableSlots, interval, selectedDate
 }
 
 export const getTimeslotByUUID = (UUID) => {
-  const retrieve = Axios.get('http://localhost:3001/timeslots/options').then((response) => {
+  const res = Axios.get('http://localhost:3001/timeslots/options').then((response) => {
     return response.data;
   })
-  return retrieve;
+  return res;
+}
+
+export const updateTimeslotByUUID = (UUID, day, time, slotsAvailableByDay) => {
+  let correctDaySlots = slotsAvailableByDay[day];
+  let slotToUpdate = correctDaySlots.filter((slot) => slot[0] === time);
+  let index;
+  for (let i = 0; i < slotsAvailableByDay[day].length; i++) {
+    if (correctDaySlots[i][0] === time) {
+      index = i;
+    }
+  }
+  // console.log(correctDaySlots, slotToUpdate, index);
+  correctDaySlots[index][1] = slotsAvailableByDay[day][index][1] - 1;
+  slotsAvailableByDay[day] = correctDaySlots;
+  console.log(slotsAvailableByDay);
+
+  Axios.put(`http://localhost:3001/timeslots/update/${UUID}`, {
+    slotsAvailableByDay: slotsAvailableByDay
+  });
 }
