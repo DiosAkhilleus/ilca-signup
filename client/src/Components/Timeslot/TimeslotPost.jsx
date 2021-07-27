e6d0a5796e7606a1a0e5f728415bee4418270a15
DiosAkhilleus <abertin1809@gmail.com>
2021-07-27 14:05:22 -0500

    modified functionality to allow changes on each day's slots

--- client/src/Components/Timeslot/TimeslotPost.jsx
+++ client/src/Components/Timeslot/TimeslotPost.jsx
@@ -11,6 +11,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import moment from 'moment';
 import TimePicker from 'rc-time-picker';
+import AdjustEntries from './AdjustEntries.jsx';
 import { postCreatedTimeslotToDB } from '../../javascript/timeslotLogic';
 import { v4 as uuidv4 } from 'uuid';
 import 'rc-time-picker/assets/index.css';
@@ -20,9 +21,12 @@ import 'react-date-range/dist/theme/default.css';
 const TimeslotPost = () => {
   const [interval, setInterval] = useState(30);
   const [unavailableSlots, setUnavailableSlots] = useState([]);
+  const [selectedDates, setSelectedDates] = useState([]);
   const [entryLimit, setEntryLimit] = useState(0);
   const [eventTitle, setEventTitle] = useState('');
   const [UUID, setUUID] = useState('');
+  const [entriesAvailable, setEntriesAvailable] = useState([]);
+  const [slotsAvailableByDay, setSlotsAvailableByDay] = useState({});
   const [timeFrom, setTimeFrom] = useState(510);
   const [startValue, setStartValue] = useState(moment('2021-01-01 08:30'));
   const [timeTo, setTimeTo] = useState(870);
@@ -34,6 +38,27 @@ const TimeslotPost = () => {
       key: 'selection',
     },
   ]);
+  useEffect(() => {
+    let days = getDates(calendar[0].startDate, calendar[0].endDate);
+    let formatted = days.map((date) => moment(date).format('YYYY-MM-DD'));
+    setSelectedDates(formatted);
+    setDateObj(formatted);
+  }, [calendar]);
+
+  useEffect(() => {
+    let replacementObj = Object.assign({}, slotsAvailableByDay);
+    for (let el in replacementObj) {
+      replacementObj[el].entriesLeft = replacementObj[el].entriesLeft.map(
+        (element, index) =>
+          replacementObj[el].unavailableSlots.indexOf(
+            replacementObj[el].entriesLeft[index][0]
+          ) === -1
+            ? [element[0], entryLimit]
+            : [element[0], 0]
+      );
+    }
+    setSlotsAvailableByDay(replacementObj);
+  }, [entryLimit]);
 
   useEffect(() => {
     const start = moment(startValue._d.toString()).format('HH:mm');
@@ -51,37 +76,27 @@ const TimeslotPost = () => {
 
   const handleTimeslotPost = () => {
     let selectedDates = getDates(calendar[0].startDate, calendar[0].endDate);
-    selectedDates = selectedDates.map((date) => moment(date).format("YYYY-MM-DD"))
+    selectedDates = selectedDates.map((date) =>
+      moment(date).format('YYYY-MM-DD')
+    );
     let uuid = uuidv4();
-    let slotsAvailableByDay = {};
-    let dailyArr = [];
-    for (let i = timeFrom; i <= timeTo-interval; i+= interval) {
-      if (unavailableSlots.filter(time => time === i).length > 0) {
-        dailyArr.push([i, 0]); 
-      } else {
-        dailyArr.push([i, entryLimit]);
-      }
-    }
-    for (let element of selectedDates) {
-      slotsAvailableByDay[element] = {};
-      slotsAvailableByDay[element].entriesLeft = dailyArr;
-      slotsAvailableByDay[element].unavailableSlots = unavailableSlots;
-    }
+
     setUUID(uuid);
     if (entryLimit === 0 || eventTitle === '') {
       alert('Please fill out all fields');
     } else {
-      postCreatedTimeslotToDB(
-        slotsAvailableByDay,
-        interval,
-        entryLimit,
-        selectedDates,
-        eventTitle,
-        timeFrom,
-        timeTo,
-        uuid 
-      );
-    };
+      console.log(slotsAvailableByDay);
+      // postCreatedTimeslotToDB(
+      //   slotsAvailableByDay,
+      //   interval,
+      //   entryLimit,
+      //   selectedDates,
+      //   eventTitle,
+      //   timeFrom,
+      //   timeTo,
+      //   uuid
+      // );
+    }
   };
   //eslint-disable-next-line
   Date.prototype.addDays = function (days) {
@@ -106,12 +121,46 @@ const TimeslotPost = () => {
       setEntryLimit(parseInt(val));
     }
   };
-  
+
+  const setDateObj = (days) => {
+    let slotsObj = {};
+    let dailyArr = [];
+    for (let i = timeFrom; i <= timeTo - interval; i += interval) {
+      if (unavailableSlots.filter((time) => time === i).length > 0) {
+        dailyArr.push([i, 0]);
+      } else {
+        dailyArr.push([i, entryLimit]);
+      }
+    }
+    for (let element of days) {
+      slotsObj[element] = {};
+      slotsObj[element].entriesLeft = dailyArr;
+      slotsObj[element].unavailableSlots = unavailableSlots;
+    }
+    setSlotsAvailableByDay(slotsObj);
+  };
+
+  const handleSetUnavailable = (el, slot) => {
+    let replacementObj = Object.assign({}, slotsAvailableByDay);
+    replacementObj[el].unavailableSlots = [
+      ...replacementObj[el].unavailableSlots,
+      slot,
+    ];
+    console.log(replacementObj[el]);
+    for (let slot of replacementObj[el].entriesLeft) {
+      if (replacementObj[el].unavailableSlots.indexOf(slot[0]) > -1) {
+        slot[1] = 0;
+      }
+    }
+    setSlotsAvailableByDay(replacementObj);
+  };
 
   return (
     <div className="timeslot-post">
       <h1>Timeslot Creator</h1>
-      <Link to="/" style={{marginBottom: 30}}>Back to home</Link>
+      <Link to="/" style={{ marginBottom: 30 }}>
+        Back to home
+      </Link>
       <div className="timeslot-options-container">
         <DateRange
           style={{ fontSize: 18 }}
@@ -195,7 +244,6 @@ const TimeslotPost = () => {
           </div>
         </div>
       </div>
-
       <div className="timeslot">
         <button
           onClick={() => {
@@ -205,18 +253,41 @@ const TimeslotPost = () => {
           Reset Unavailable Slots
         </button>
         <h3>Click individual slots below to make them unavailable</h3>
-        <SlotPicker
-          interval={interval}
-          unavailableSlots={unavailableSlots}
-          selected_date={new Date()}
-          from={timeFrom}
-          to={timeTo}
-          onSelectTime={(slot) => {
-            setUnavailableSlots([...unavailableSlots, slot]);
-          }}
-        />
+        {Object.keys(slotsAvailableByDay).map((el, index) => (
+          <div key={index} className="admin-slot-container">
+            <div style={{ textAlign: 'center', marginBottom: 10 }}>
+              <strong>{moment(el).format('D MMMM YYYY')}</strong>
+            </div>
+            <div style={{ marginBottom: 8 }}>
+              <AdjustEntries
+                setSlotsAvailableByDay={setSlotsAvailableByDay}
+                slotsAvailableByDay={slotsAvailableByDay}
+                element={el}
+                index={index}
+              />
+            </div>
+            <div style={{ marginBottom: 15 }}>
+              <SlotPicker
+                key={[el, index]}
+                interval={interval}
+                unavailableSlots={slotsAvailableByDay[el].unavailableSlots}
+                selected_date={new Date()}
+                from={timeFrom}
+                to={timeTo}
+                onSelectTime={(slot) => {
+                  handleSetUnavailable(el, slot);
+                }}
+              />
+            </div>
+          </div>
+        ))}
       </div>
-      <Button variant="contained" color="primary" style={{ marginBottom: 20, marginTop: 40 }} onClick={() => handleTimeslotPost()}>
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginBottom: 20, marginTop: 40 }}
+        onClick={() => handleTimeslotPost()}
+      >
         Submit Timeslot Sheet
       </Button>
       {UUID !== '' ? (
@@ -233,7 +304,9 @@ const TimeslotPost = () => {
       ) : (
         ''
       )}
-      <hr style={{border: '1px solid black', width: '100%', marginBottom: 40}}/>
+      <hr
+        style={{ border: '1px solid black', width: '100%', marginBottom: 40 }}
+      />
     </div>
   );
 };
