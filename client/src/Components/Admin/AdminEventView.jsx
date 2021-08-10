7fda3d869fa1f51f9035e800904250f653e7c783
DiosAkhilleus <abertin1809@gmail.com>
2021-08-10 18:08:34 -0500

    worked on Admin event viewing

--- client/src/Components/Admin/AdminEventView.jsx
+++ client/src/Components/Admin/AdminEventView.jsx
@@ -6,27 +6,65 @@ import { getSignupByEventNum } from '../../javascript/adminLogic';
 const ViewEvent = () => {
   const { ilcaNum } = useParams();
   const [currentSignup, setCurrentSignup] = useState({});
+  const [dates, setDates] = useState([]);
+  const [slotsByDay, setSlotsByDay] = useState({});
+  const [registered, setRegistered] = useState([]);
 
   useEffect(() => {
-    getSignupByEventNum(ilcaNum).then((results) =>
-      setCurrentSignup(results[0])
-    );
+    getSignupByEventNum(ilcaNum).then((results) => {
+      setCurrentSignup(results[0]);
+    });
     //eslint-disable-next-line
   }, []);
+
+  useEffect(() => {
+    // setDates(Object.keys(currentSignup.slotsAvailableByDay));
+    if (currentSignup.slotsAvailableByDay) {
+      setDates(Object.keys(currentSignup.slotsAvailableByDay));
+      setSlotsByDay(currentSignup.slotsAvailableByDay);
+      setRegistered(currentSignup.inspectionReqs);
+    }
+  }, [currentSignup]);
 
   return (
     <div>
       <Link to="/admin">Back to Admin</Link>
       <div>
-        <h3>Currently Signed Up Sailors</h3>
-        {currentSignup.inspectionReqs &&
-        currentSignup.inspectionReqs.length > 0 ? (
-          currentSignup.inspectionReqs.map((el, index) => (
-            <div key={index}>{el.sailorID}</div>
+        <div className="title">
+          {currentSignup.eventTitle ? (
+            <strong>
+              <div>{currentSignup.eventTitle}</div>
+            </strong>
+          ) : (
+            ''
+          )}
+        </div>
+        <h3>Current Inspection Signups</h3>
+        {registered.length > 0 ? (
+          registered.map((el, index) => (
+            <div key={index}>
+              {el.sailorID} – {el.name.familyName}, {el.name.firstName} –{' '}
+              {el.day}
+            </div>
           ))
         ) : (
           <div>No Sailors Currently Signup Up</div>
         )}
+      </div>
+      <br />
+      <div>
+        {dates.length > 0
+          ? dates.map((date, index) => (
+              <div key={index}>
+                {date}
+                {slotsByDay
+                  ? slotsByDay[date].entriesLeft.map((info, ind) => (
+                      <div>{info[0]}</div>
+                    ))
+                  : ''}
+              </div>
+            ))
+          : ''}
       </div>
     </div>
   );
