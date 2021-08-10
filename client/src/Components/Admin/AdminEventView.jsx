c04601e1065251730661a07c5abd4b5393c29c4d
DiosAkhilleus <abertin1809@gmail.com>
2021-08-10 13:45:55 -0500

    worked on info display for admin view

--- client/src/Components/Admin/AdminEventView.jsx
+++ client/src/Components/Admin/AdminEventView.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getSignupByEventNum } from '../../javascript/adminLogic';
 
 const ViewEvent = () => {
-
   const { ilcaNum } = useParams();
+  const [currentSignup, setCurrentSignup] = useState({});
+
+  useEffect(() => {
+    getSignupByEventNum(ilcaNum).then((results) =>
+      setCurrentSignup(results[0])
+    );
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <div>
-      {ilcaNum}
+      <Link to="/admin">Back to Admin</Link>
+      <div>
+        <h3>Currently Signed Up Sailors</h3>
+        {currentSignup.inspectionReqs &&
+        currentSignup.inspectionReqs.length > 0 ? (
+          currentSignup.inspectionReqs.map((el, index) => (
+            <div key={index}>{el.sailorID}</div>
+          ))
+        ) : (
+          <div>No Sailors Currently Signup Up</div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ViewEvent
+export default ViewEvent;
