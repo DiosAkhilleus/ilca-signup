39e83010d8c588c8a87ac2b3df228986b1021bc0
DiosAkhilleus <abertin1809@gmail.com>
2021-08-11 11:32:07 -0500

    added new put request logic to admin

--- client/src/javascript/adminLogic.js
+++ client/src/javascript/adminLogic.js
@@ -3,9 +3,47 @@ import Axios from 'axios';
 export const getSignupByEventNum = (ilcaNum) => {
   const res = Axios.get('http://localhost:3001/timeslots/options').then(
     (response) => {
-      return response.data.filter(signup => signup.ilcaNum === ilcaNum);
+      return response.data.filter((signup) => signup.ilcaNum === ilcaNum);
     }
   );
-  // const filtered = res.filter((element) => element.ilcaNum === ilcaNum)
   return res;
-}
+};
+
+export const updateSailorInspection = (
+  sailorID,
+  timeFrom,
+  timeTo,
+  day,
+  registered,
+  ilcaNum,
+  slotsByDay,
+  slotsIndex
+) => {
+  let nonMatchingArr = registered.filter(
+    (inspec) => inspec.sailorID !== sailorID
+  );
+  let matchingObj = registered.filter(
+    (inspec) => inspec.sailorID === sailorID
+  )[0];
+  let replacementInspecObj = Object.assign(matchingObj, {
+    time: timeTo,
+    day: day,
+  });
+  let inspectionReqs = [...nonMatchingArr, replacementInspecObj];
+  let replacementEntriesLeft = slotsByDay[day].entriesLeft.map(
+    (element, index) =>
+      index === slotsIndex
+        ? [element[0], element[1] - 1]
+        : element[0] === timeFrom
+        ? [element[0], element[1] + 1]
+        : element
+  );
+
+  let replacementSlotsObj = Object.assign(slotsByDay, {});
+  replacementSlotsObj[day].entriesLeft = replacementEntriesLeft;
+
+  Axios.put(`http://localhost:3001/timeslots/updateinspecs/${ilcaNum}`, {
+    inspectionReqs: inspectionReqs,
+    slotsAvailableByDay: replacementSlotsObj,
+  });
+};
