8d74789aeca3dd4b2c303337cea67c8a9301d511
DiosAkhilleus <abertin1809@gmail.com>
2021-09-11 10:40:04 -0500

    modified routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Components/Home.jsx'
-import About from './Components/About.jsx';
 import Admin from './Components/Admin/Admin.jsx';
 import NotFound from './Components/NotFound.jsx';
 import TimeslotPost from './Components/Admin/TimeslotPost.jsx';
 import AdminEventView from './Components/Admin/AdminEventView.jsx';
-import PostSailor from './Components/Sailor/PostSailor.jsx';
-import DisplaySailors from './Components/Sailor/DisplaySailors.jsx';
 import SailorEventView from './Components/Timeslot/SailorEventView';
 
 function App() {
@@ -16,14 +12,10 @@ function App() {
 
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/admin' component={Admin}/>
+        <Route exact path='/' component={Admin} />
         <Route exact path='/admin/create' component={TimeslotPost} />
         <Route path='/signup/:id' children={<SailorEventView />}/>
         <Route path='/admin/event/:ilcaNum' children={<AdminEventView />}/>
-        <Route exact path='/postsailor' component={PostSailor} />
-        <Route exact path='/displaysailors' component={DisplaySailors} />
         <Route path='/' component={NotFound}/>
       </Switch>
     </BrowserRouter>
